ba3a773a2f0016bed874db1671d0155ec83cc158
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-08-30 17:52:07 +0530

    schema created

---
+++ one-shot/backend/models/college.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const collegeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    founded: {
+        type: Number,
+        required: true,
+        trim: true,
+        minlength: 4
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    no_of_students: {
+        type: Number,
+        required: true
+    },
+    courses: {
+        type: [String],
+        required: true
+    }
+    
+}, {
+  timestamps: true,
+});
+
+const College = mongoose.model('College', collegeSchema);
+
+module.exports = College;
---
+++ one-shot/backend/models/student.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema(
+{
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  batch_year:{
+    type: Number,
+    max: 2026,
+    required: true,
+    trim: true,
+    minlength: 4
+  },
+  college_id: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 9
+  },
+  skills: {
+    type: [String],
+    required: true
+  }
+}, 
+{
+  timestamps: true,
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
